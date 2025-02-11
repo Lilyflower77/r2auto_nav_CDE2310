f09ab81e6e09f4d4240af953baa34466e283c548
Hong-yiii <hongyilin.mail@gmail.com>
2025-02-11 22:52:51 +0800

    added lidar_and_servo.py

--- assignments/E2.py
+++ assignments/E2.py
@@ -4,20 +4,20 @@ import RPi.GPIO as GPIO
 
 # GPIO.setmode(GPIO.BOARD)    # number on circle
 GPIO.setmode(GPIO.BCM)  # number beside GPIO
-servo_pin = 21
-GPIO.setup(servo_pin, GPIO.OUT)
+solenoid_pin = 21
+GPIO.setup(solenoid_pin, GPIO.OUT)
 
 def setPin(userInput):
     if userInput == True:
-        GPIO.output(servo_pin, GPIO.HIGH)
+        GPIO.output(solenoid_pin, GPIO.HIGH)
         time.sleep(1)
-        GPIO.output(servo_pin, GPIO.LOW)
+        GPIO.output(solenoid_pin, GPIO.LOW)
     else: 
         print("Not launching")
 try:
     while True:
         userInput = bool(input("Do you want to launch?"))
-        GPIO.output(servo_pin, GPIO.LOW)
+        GPIO.output(solenoid_pin, GPIO.LOW)
         setPin(userInput)
 except KeyboardInterrupt:
     GPIO.cleanup()  # remove all setup
