1eef8a49e53d78a2b1d4e19e5aac9b14ac955b18
Hong-yiii <hongyilin.mail@gmail.com>
2025-03-25 22:41:52 +0800

    bringup with cartographer

--- bot_bestie/launch/nav2_bringup.py
+++ bot_bestie/launch/nav2_bringup.py
@@ -1,34 +1,20 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Darby Lim
+# Copyright 2019 Open Source Robotics Foundation
+# Licensed under the Apache License, Version 2.0, etc.
 
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 
-
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    # This is used if you also want to load an initial map or do pure localization
     map_dir = LaunchConfiguration(
         'map',
         default=os.path.join(
@@ -36,6 +22,7 @@ def generate_launch_description():
             'map',
             'map.yaml'))
 
+    # Nav2 param file for TurtleBot3 (e.g. burger.yaml, waffle.yaml, etc.)
     param_file_name = TURTLEBOT3_MODEL + '.yaml'
     param_dir = LaunchConfiguration(
         'params_file',
@@ -44,42 +31,70 @@ def generate_launch_description():
             'param',
             param_file_name))
 
-    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    nav2_launch_file_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'launch'
+    )
 
     rviz_config_dir = os.path.join(
         get_package_share_directory('nav2_bringup'),
         'rviz',
-        'nav2_default_view.rviz')
+        'nav2_default_view.rviz'
+    )
 
     return LaunchDescription([
+        # Arguments
         DeclareLaunchArgument(
             'map',
             default_value=map_dir,
-            description='Full path to map file to load'),
+            description='Full path to map file to load (if doing map-based localization)'),
 
         DeclareLaunchArgument(
             'params_file',
             default_value=param_dir,
-            description='Full path to param file to load'),
+            description='Full path to Nav2 param file to load'),
 
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 
+        # Path to Cartographer's config files
+        cartographer_config_dir = os.path.join(
+            get_package_share_directory('turtlebot3_cartographer'),
+            'config'
+        ),
+
+
+        # 2) Nav2 bringup (runs map_server, planner, controller, etc.)
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
             launch_arguments={
                 'map': map_dir,
                 'use_sim_time': use_sim_time,
-                'params_file': param_dir}.items(),
+                'params_file': param_dir
+            }.items(),
         ),
 
+        # cartographer_node
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            name='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-configuration_directory', cartographer_config_dir,
+                       '-configuration_basename', configuration_basename]
+        ),
+
+        # 3) RViz
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             arguments=['-d', rviz_config_dir],
             parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+            output='screen'
+        ),
     ])
